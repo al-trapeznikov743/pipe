e49bab29729c5f595cc422e437c4e02b42732ca3
al_trapeznikov <al.trapeznikov.743@gmail.com>
2024-11-16 23:36:58 +0400

    backend refactor

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@nestjs/common';
 @Injectable()
 export class AppService {
   getUsers() {
-    return [{id: 1, name: 'John'}];
+    return [{id: 2, name: 'John'}];
   }
 };
